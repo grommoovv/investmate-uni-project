e6c9067399a703a702579515c3b70539e49f4f34
GEORGE GROMOV <gromov.dev13@gmail.com>
2023-05-26 01:18:01 +0300

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+    colors: {
+      white: '#fff',
+      light: '#C6C6C9',
+      dark: '#1a1a29',
+      green: '#2ED42B',
+    },
+  },
+  plugins: [],
+}
